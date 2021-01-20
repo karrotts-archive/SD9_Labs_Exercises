cc53f3f64426fd33c4bdbfb5c93b31f3e623468a
wes <Wesleymiller1996@gmail.com>
2021-01-20 11:28:27 -0600

    Add project files.

---
+++ LabsHome/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LabsHome
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

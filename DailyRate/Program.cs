600e2a42ec9256655d04164e9f67685eb91d7ae0
wes <Wesleymiller1996@gmail.com>
2021-01-20 11:49:30 -0600

    Added Lab 2A projects

---
+++ DailyRate/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DailyRate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

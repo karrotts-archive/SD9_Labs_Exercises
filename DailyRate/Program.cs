c78eebd0be8d46c3a44b3e602d2cde29531db9c1
wes <Wesleymiller1996@gmail.com>
2021-01-20 19:16:52 -0600

    completed LAB2A

--- DailyRate/Program.cs
+++ DailyRate/Program.cs
@@ -6,7 +6,31 @@ namespace DailyRate
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            (new Program()).run();
+        }
+
+        void run()
+        {
+            double dailyRate = readDouble("Enter your daily rate: ");
+            int noOfDays = readInt("Enter the number of days: ");
+            writeFee(calculateFee(dailyRate, noOfDays));
+        }
+
+        private void writeFee(double v) => Console.WriteLine($"The consultant's fee is: {v * 1.1}");
+        private double calculateFee(double dailyRate, int noOfDays) => dailyRate * noOfDays;
+
+        private int readInt(string v)
+        {
+            Console.Write(v);
+            string line = Console.ReadLine();
+            return int.Parse(line);
+        }
+
+        private double readDouble(string v)
+        {
+            Console.Write(v);
+            string line = Console.ReadLine();
+            return double.Parse(line);
         }
     }
 }
--- Factorial/Program.cs
+++ Factorial/Program.cs
@@ -6,7 +6,28 @@ namespace Factorial
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Write("Please enter a positive integer: ");
+            string inputValue = Console.ReadLine();
+            long factorialValue = CalculateFactorial(inputValue);
+            Console.WriteLine($"Factorial({inputValue}) is {factorialValue}");
+        }
+
+        static long CalculateFactorial(string input)
+        {
+            int inputValue = int.Parse(input);
+            long factorial(int dataValue)
+            {
+                if (dataValue == 1)
+                {
+                    return 1;
+                }
+                else
+                {
+                    return dataValue * factorial(dataValue - 1);
+                }
+            }
+            long factorialValue = factorial(inputValue);
+            return factorialValue;
         }
     }
 }
